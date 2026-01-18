f911c5a8f48c85de7e9d2caee58e1b73dcb4ed0c
himanshutyagi0006-ai <himanshutyagi0006@gmail.com>
2026-01-18 19:37:57 +0530

    Add files via upload

---
+++ Scipt1.js
@@ -0,0 +1,5 @@
+function showLove() {
+    const response = document.getElementById("response");
+    response.innerHTML = "Bas ek haan chahiye… baaki zindagi meri ❤️";
+    response.style.animation = "pulse 1.5s infinite";
+}
